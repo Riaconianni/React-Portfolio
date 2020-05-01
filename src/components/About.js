8640d0bd8ce76f227a46eb7a11448d38bd0c7ac8
Rachel Iaconianni <iaconiannir@gmail.com>
2020-05-01 12:00:39 -0400

    edits

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import Me from "../rachel.jpg";
 function About() {
   return (
     <Container>
-      <Row xs={12} md={4} className="justify-content-center">
+      <Row xs={12} md={12} className="justify-content-center">
         <Figure>
           <Figure.Image
             width={171}
@@ -17,16 +17,16 @@ function About() {
         </Figure>
       </Row>
       <Row>
-        <Col xs={12} md={11} className="text-center">
+        <Col xs={12} md={12} className="text-center">
           <h2>Rachel Iaconianni</h2>
         </Col>
       </Row>
-      <Row xs={12} md={8} className="justify-content-center ml-auto">
+      <Row xs={12} md={12} className="justify-content-center ml-auto">
         <Container className="mt-3">
           <p>
             I am a Fullstack Developer that attended a Web Development Bootcamp
-            at Rutgers University. Currently, I am a Configuration Management
-            Specialist at the FAA Tech Center in Atlantic City, NJ. Before I
+            at Rutgers University. Currently, I am a Program Analyst for the Configuration Management
+            Team at the FAA Tech Center in Atlantic City, NJ. Before I
             started working for the FAA Tech Center, I was a Personal Banker for
             a small bank called Univest. I was in the banking industry for 7
             years. Also, I am currently a board member for Big Brothers Big
