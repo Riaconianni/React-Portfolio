bcb3506f1d6f210b209240343713db93572b5997
Rachel Iaconianni <iaconiannir@gmail.com>
2020-05-01 11:53:41 -0400

    changed pic and about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import { Row, Col, Container, Image, Figure, Card } from 'react-bootstrap';
-import Me from '../me.jpg'
+import React from "react";
+import { Row, Col, Container, Image, Figure, Card } from "react-bootstrap";
+import Me from "../rachel.jpg";
 
 function About() {
   return (
     <Container>
-  <Row xs={12} md={4} className="justify-content-center">
-      <Figure>
-      <Figure.Image width={171} height={180} alt="Picture of Rachel" src={Me} roundedCircle/>
-      </Figure>
-  </Row>
-  <Row>
-    <Col xs={12} md={11} className="text-center">
-    <h2>Rachel Iaconianni</h2>
-    </Col>
-  </Row>
-  <Row  xs={12} md={8} className="justify-content-center ml-auto">
-    <Card>
-      <Card.Title>About Me</Card.Title>
-      <Card.Body> I am currently attending a coding bootcamp at Rutgers
-                University. Recently, I started a new position at the FAA Tech
-                Center as a Configuration Management Specialist. Before I
-                started working for the FAA Tech Center, I was a Personal Banker
-                for a small bank called Univest. I was in the banking industry
-                for 7 years. Also, I am currently a board member for Big
-                Brothers Big Sisters of Atlantic and Cape May Counties.</Card.Body>
-    </Card>
-  </Row>
-</Container>
-  )
-};
+      <Row xs={12} md={4} className="justify-content-center">
+        <Figure>
+          <Figure.Image
+            width={171}
+            height={180}
+            alt="Picture of Rachel"
+            src={Me}
+            roundedCircle
+          />
+        </Figure>
+      </Row>
+      <Row>
+        <Col xs={12} md={11} className="text-center">
+          <h2>Rachel Iaconianni</h2>
+        </Col>
+      </Row>
+      <Row xs={12} md={8} className="justify-content-center ml-auto">
+        <Container className="mt-3">
+          <p>
+            I am a Fullstack Developer that attended a Web Development Bootcamp
+            at Rutgers University. Currently, I am a Configuration Management
+            Specialist the FAA Tech Center in Atlantic City, NJ. Before I
+            started working for the FAA Tech Center, I was a Personal Banker for
+            a small bank called Univest. I was in the banking industry for 7
+            years. Also, I am currently a board member for Big Brothers Big
+            Sisters of Atlantic and Cape May Counties.
+          </p>
+        </Container>
+      </Row>
+    </Container>
+  );
+}
 
 export default About;
